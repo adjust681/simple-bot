8de8da4f1250cc775aa1a608bf0276284986f87d
adjust681 <60233665+adjust681@users.noreply.github.com>
2024-03-24 21:44:52 +0300

    Update IdentificatorBot.php

--- admin/IdentificatorBot.php
+++ admin/IdentificatorBot.php
@@ -4,10 +4,10 @@
 	{
 		static string $bot_token    = '6xxxxxxx4:XXXXX-XXXXXXXXXXXXXXXXXXXXXXX_0';
 		static string $telegram_url = 'https://api.telegram.org/bot';
-        static string $bot_url      = 'https://api.telegram.org/bot6xxxxxxx4:XXXXX-XXXXXXXXXXXXXXXXXXXXXXX_0';
-        static string $bot_path     = '/simple-bot/bot.php';
-        static string $ssl_cert     = '@/etc/ssl/certificate.pem';
+                static string $bot_url      = 'https://api.telegram.org/bot6xxxxxxx4:XXXXX-XXXXXXXXXXXXXXXXXXXXXXX_0';
+                static string $bot_path     = '/simple-bot/bot.php';
+                static string $ssl_cert     = '@/etc/ssl/certificate.pem';
 
-        static string $linguage_default = 'ru';
-        static int    $admin_id         = 000000000;
+                static string $linguage_default = 'ru';
+                static int    $admin_id         = 000000000;
 	}
